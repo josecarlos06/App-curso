f7610f84bb8274928ed149454b18d7c9f13cebf4
josecarlos06 <l181130425@gamadero.tecnm.mx>
2022-11-16 19:57:00 -0600

    style

--- client/index.js
+++ client/index.js
@@ -1,6 +1,4 @@
 // Cargado estilos
 import './stylesheet/style.css';
-import 'material-icons/iconfont/material-icons.css';
-// import 'materialize-css/dist/css/materialize.css';
-// import 'materialize-css/dist/js/materialize';
+
 M.AutoInit();
