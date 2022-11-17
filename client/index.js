b3fc6613e54b9193dea5211a028f80a42c71288a
josecarlos06 <l181130425@gamadero.tecnm.mx>
2022-11-17 11:00:36 -0600

    Style Gob

--- client/index.js
+++ client/index.js
@@ -1,9 +1,4 @@
 // Cargado estilos
 import './stylesheet/style.css';
-<<<<<<< HEAD
 import './stylesheet/navbar.css';
 import './stylesheet/footer.css';
-=======
-
-M.AutoInit();
->>>>>>> 1-entity-creation
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,15 @@
-const toggle=document.querySelector('#toggle');
-const navUl=document.querySelector('.nav__ul')
 menu();
 function menu(){
+   const toggle=document.querySelector('#toggle');
+   const navUl=document.querySelector('.nav__ul')
    toggle.addEventListener('click',()=>{
+      const navIcon=document.querySelector('.nav__icon');
       if(navUl.classList.contains('actived')){
          navUl.classList.remove('actived')
+         navIcon.textContent = 'menu'
       }else{
          navUl.classList.add('actived')
+         navIcon.textContent = 'close'
       }
    })
 };
