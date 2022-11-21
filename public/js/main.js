bd2a84bafc06c623043dd39ee820099b20705713
josecarlos06 <l181130425@gamadero.tecnm.mx>
2022-11-21 15:32:01 -0600

    style

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,3 @@
-$('.owl-carousel').owlCarousel({
-   loop:true,
-   margin:10,
-   nav:true,
-   responsive:{
-       0:{
-           items:1
-       },
-       600:{
-           items:1
-       },
-       1000:{
-           items:1
-       }
-   }
-})
 menu();
 function menu(){
    const toggle=document.querySelector('#toggle');
